6fcee351794c569cbab9c30aa33a4bfd7c14b570
Youssef Kadaoui Abbassi <youssef@rebright.io>
2024-06-01 07:27:16 +0200

    feat: add next locales

---
+++ apps/web/src/app/[locale]/page.tsx
@@ -0,0 +1,10 @@
+import MainHeader from '@/components/headers/mainHeader';
+
+export default async function Index() {
+  return (
+    <>
+      <MainHeader />
+      <div className="text-3xl">Test</div>
+    </>
+  );
+}
