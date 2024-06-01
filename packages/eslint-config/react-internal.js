d69966ab1a17a8a854522ab880eb4494ad3a6fa4
Youssef Kadaoui Abbassi <youssef@rebright.io>
2024-06-01 03:13:02 +0200

    feat (ui): add prettier

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -10,8 +10,17 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
+  extends: [
+      "eslint:recommended",
+      "prettier",
+      "eslint-config-turbo",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "only-warn"
+  ],
   globals: {
     React: true,
     JSX: true,
