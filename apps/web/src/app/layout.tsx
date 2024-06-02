7a564602394219c13ac116692c6b9fc801181cee
Youssef Kadaoui Abbassi <youssef@rebright.io>
2024-06-02 02:17:56 +0200

    fix: add declaration false to nextjs tsconfig to avoid typescript error

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,12 +2,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
   title: 'BackFast - Studio',
   description: 'BackFast - Studio',
 };
