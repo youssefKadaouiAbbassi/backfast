4119a7a6c5bc14d34b43c544374565b5e6b770a5
Youssef Kadaoui Abbassi <youssef@rebright.io>
2024-06-09 02:58:29 +0200

    feat: add env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
