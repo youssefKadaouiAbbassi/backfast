3b801b9272697cdc1b030f9af7b343bb68295d32
Youssef Kadaoui Abbassi <youssef@rebright.io>
2024-06-10 19:51:46 +0200

    fix: update eslint and tsconfig config

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -11,9 +11,9 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "prettier",
-        "eslint-config-turbo",
         "plugin:@typescript-eslint/recommended",
         "plugin:prettier/recommended",
+        "turbo",
     ],
     plugins: [
         "@typescript-eslint",
