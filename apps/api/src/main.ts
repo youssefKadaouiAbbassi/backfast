4bef8c281e767e0157c9708844422f3cb2b40859
Youssef Kadaoui Abbassi <youssefabbassi4@gmail.com>
2024-06-13 01:44:59 +0200

    feat: add swagger to api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    const sw = await import('@nestjs/swagger');
+
+    const config = new sw.DocumentBuilder()
+      .setTitle(process.env.PROJECT_NAME)
+      .setDescription(`${process.env.PROJECT_NAME} API Documentation`)
+      .setVersion('0.0.1')
+      .addBearerAuth({ bearerFormat: 'JWT', type: 'http' })
+      .build();
+    const document = sw.SwaggerModule.createDocument(app, config);
+    sw.SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(process.env.API_PORT || 9000);
 }
 bootstrap();
