f836f0b509bdaa4d5798454115cff8d8f2adef99
Youssef Kadaoui Abbassi <youssefabbassi4@gmail.com>
2024-06-13 02:42:11 +0200

    feat: add db docker-compose file and env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
     }),
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
